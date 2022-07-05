ebe11e236c22543a1d42cdea3a23392f581b9994
Marco Lopez <lopez.marco@live.fr>
2022-07-05 17:03:48 +0200

    change file path

--- harvai/params.py
+++ harvai/params.py
@@ -5,28 +5,16 @@ def get_path_data(path):
     if os.path.basename(path) == 'harvai-lite':
         return "raw_data/LEGITEXT000006074228.pdf"
     else:
-        return "../raw_data/LEGITEXT000006074228.pdf"
+        return "raw_data/LEGITEXT000006074228.pdf"
 
 def get_path_json(path):
     if os.path.basename(path) == 'harvai-lite':
         return "raw_data/data_preproc.json"
     else:
-        return "../raw_data/data_preproc.json"
+        return "raw_data/data_preproc.json"
 
 def get_path_json_digits(path):
     if os.path.basename(path) == 'harvai-lite':
         return "raw_data/data_preproc_digits.json"
     else:
-        return "../raw_data/data_preproc_digits.json"
-
-def get_path_faiss(path):
-    if os.path.basename(path) == 'harvai-lite':
-        return "raw_data/faiss.index"
-    else:
-        return "../raw_data/faiss.index"
-
-def get_path_retriever(path):
-    if os.path.basename(path) == 'harvai-lite':
-        return "raw_data/retriever.pt"
-    else:
-        return "../raw_data/retriever.pt"
+        return "raw_data/data_preproc_digits.json"
